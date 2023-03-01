89e1a886f0b291c5d7f76bd2d409f4915eb83b2e
gsantini-ube <gsantini@ubeswap.org>
2023-03-01 16:49:27 -0500

    feat: convert ubeswap token type to uniswap token type and vice-versa

--- packages/ubeswap-components/src/components/SearchModal/CommonBases.tsx
+++ packages/ubeswap-components/src/components/SearchModal/CommonBases.tsx
@@ -1,11 +1,11 @@
 import { ChainId, Token } from '@ubeswap/sdk'
-import { useAllTokens } from 'hooks/Tokens'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { Text } from 'rebass'
 import styled from 'styled-components'
 
 import { SUGGESTED_BASES } from '../../constants'
+import { useAllTokens } from '../../hooks/Tokens'
 import { AutoColumn } from '../Column'
 import CurrencyLogo from '../CurrencyLogo'
 import QuestionHelper from '../QuestionHelper'
@@ -51,7 +51,10 @@ export default function CommonBases({
           const selected = selectedCurrency instanceof Token && selectedCurrency.address === token.address
           return (
             <BaseWrapper onClick={() => !selected && onSelect(token)} disable={selected} key={token.address}>
-              <CurrencyLogo currency={allTokens[token.address] ?? token} style={{ marginRight: 8 }} />
+              <CurrencyLogo
+                currency={allTokens[token.address] ? allTokens[token.address] : token}
+                style={{ marginRight: 8 }}
+              />
               <Text fontWeight={500} fontSize={16}>
                 {token.symbol}
               </Text>
